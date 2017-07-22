ca2a4e0c828b84a0dffd1ea9fb0ba71c4a66cbd5
elena-orlova <d9:36:f3:7b:a9:74:80:b3:df:8b:46:76:10:f9:d9:85>
2017-07-22 11:35:09 +0200

    Initial Commit

---
+++ Pocket ColliderTests/Pocket_ColliderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pocket_ColliderTests.swift
+//  Pocket ColliderTests
+//
+//  Created by Елена Орлова on 22/07/2017.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import XCTest
+@testable import Pocket_Collider
+
+class Pocket_ColliderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
