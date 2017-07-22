61c29d7e9055a4c0c16bf93a839d070dbde60fa5
elena-orlova <d9:36:f3:7b:a9:74:80:b3:df:8b:46:76:10:f9:d9:85>
2017-07-22 15:26:17 +0200

    particle type and description

--- Pocket Collider/ViewController.swift
+++ Pocket Collider/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var particleLabel:UITextView!
+    @IBOutlet weak var particleDescr:UILabel!
+    
+    var particleName:String
+    let particleNameString:String = String(format:"You discovered %s ", self.particleName)
+    
+    //var pVC = ViewController(withParticle : "Electron")
+    func init(withParticle name:string) {
+        super.init()
+        self.particleName = name
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.particle_name.text = self.particleName
+        
+        if (self.particleName == "Electron"){
+            self.particle_desc.text = "A negatively charged particle that orbits atoms, the building blocks of matter"
+        }
+        
+        if (self.particleName == "Muon"){
+            self.particle_desc.text = "A heavier cousin of the electron with same charge and spin. It is present in cosmic rays."
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
